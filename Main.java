23a2a6dd935bcf9cb7e1f00daf1ff6ccb978a6bc
levanhuy12345 <lehuy1234987@gmail.com>
2023-09-27 11:09:46 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PC
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = null;
+        try {
+            int countLine = 0;
+            br = new BufferedReader(new FileReader(new File("C:\\Users\\PC\\Downloads\\src (3).zip\\src\\normalizetext\\input.txt")));
+            while (br.readLine() != null) {
+                countLine++;
+            }
+            br.close();
+
+            int count = 1;
+            br = new BufferedReader(new FileReader(new File("C:\\Users\\PC\\Downloads\\src (3).zip\\src\\normalizetext\\input.txt")));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\PC\\Downloads\\src (3).zip\\src\\normalizetext\\output.txt", true)));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                line = normalizeOneSpace(line);
+                line = normalizeSpecialCharacters(line);
+                line = capitalizeAfterDot(line);
+                line = removeSpacesInsideQuotes(line);
+                line = capitalizeFirstCharacter(line);
+                line = addDotAtEnd(line);
+
+                pw.print(line);
+
+                if (count < countLine) {
+                    pw.print(System.getProperty("line.separator"));
+                }
+                count++;
+            }
+
+            br.close();
+            pw.close();
+
+            System.out.println("Normalize successful. 😊");
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found. ❌");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static String normalizeOneSpace(String line) {
+        return line;
+    }
+
+    private static String normalizeSpecialCharacters(String line) {
+        return line;
+    }
+
+    private static String capitalizeAfterDot(String line) {
+        return line;
+    }
+
+    private static String removeSpacesInsideQuotes(String line) {
+        return line;
+    }
+
+    private static String capitalizeFirstCharacter(String line) {
+        return line;
+    }
+
+    private static String addDotAtEnd(String line) {
+        return line;
+    }
+}
